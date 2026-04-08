f8100c7aeb157d15fe228ea4c1640579fcb1febd
Ruben <rubencarreteropelaez@gmail.com>
2026-04-08 19:22:46 +0200

    Quinto Commit

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 require("dotenv").config();
-
-const PORT = process.env.PORT || 5000;
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-
-const app = express();
-app.use(cors()); 
-app.use(express.json()); 
-
 connectDB();
 
+const app = express();
+const allowedOrigins = [
+  "https://front-end-uj5m.onrender.com",
+  "http://localhost:3000"           
+];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `La URL ${origin} no está permitida por CORS`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+app.use(express.json());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/favorites", require("./routes/favoriteRoutes"));
 app.use("/api/teams", require("./routes/teamRoutes"));
@@ -19,5 +30,7 @@ app.use((req, res) => {
   res.status(404).send("Ruta no encontrada");
 });
 
-app.listen(PORT, () => {console.log("Servidor corriendo 🔥"); 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo 🔥 en puerto ${PORT}`);
 });
